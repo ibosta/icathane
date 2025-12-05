851b821c0a1121b5cd4a735d56d2cbb7c09c1d60
Saido <saidoteknoloji@gmail.com>
2025-12-05 18:06:22 +0300

    Create AbdullahSaidAkyıldız.cs
    yok

---
+++ 2025-2026lise/dongu-icinde-dongu/AbdullahSaidAkyıldız.cs
@@ -0,0 +1,25 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        int j =0;
+        while(i<3)
+        {
+            j= 0 ;
+            while(j<5)
+        {
+            Console.Write("*");
+            j++;
+        }
+            Console.WriteLine();
+            i++;
+        }
+        
+    }
+}
