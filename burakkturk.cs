872ce3eb9c1c8b2ae5b651a0a463aaf7101eaf13
Burakkayra41 <yattaraabuzer@gmail.com>
2025-12-05 18:07:57 +0300

    Add files via upload

---
+++ burakkturk.cs
@@ -0,0 +1,26 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        int j = 0;
+        while (i<4)
+        {
+            j=0;
+            while (j<4)
+            {
+                Console.Write("*");
+                j++;
+            }   
+                Console.WriteLine();
+                i++;
+            
+        }
+        
+    }
+}
