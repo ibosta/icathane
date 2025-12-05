19bd39c82a791ffeb0ae440d99b052df623ce6b8
Faruk Sözen <frukofaruking@gmail.com>
2025-12-05 18:18:20 +0300

    Create Faruk Sözen.cs

---
+++ 2025-2026lise/dongu-icinde-dongu/Faruk Sözen.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+public class donguicidongu
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        int j = 0;
+        while(i < 3)
+        {
+            j = 0 ;
+            while(j < 4)
+        {
+            Console.Write("* ");
+            j++;
+        }
+            Console.WriteLine();
+            i++;
+        }
+        
+    }
+}
