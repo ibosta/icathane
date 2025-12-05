8b59f9d5f09e5d0a1626549f6127369ded224a33
gazali4153 <kurtogluahmetgazali@gmail.com>
2025-12-05 18:14:26 +0300

    Create Ahmet Gazali Kurtoğlu.cs

---
+++ 2025-2026lise/dongu-icinde-dongu/Ahmet Gazali Kurtoğlu.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        int j = 0;
+        while(i < 4)
+        {
+            j=0;
+            while(j < 4)
+            {
+                Console.Write("*");
+                j++;
+            }
+                Console.WriteLine();
+                i++;
+        }
+}
+}
